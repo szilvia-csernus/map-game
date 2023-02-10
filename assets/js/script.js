27166dc5be0be690cc92c2583f8e2be34f7ed897
Szilvia Csernus <csernus.szilvi@gmail.com>
2023-02-10 12:46:38 +0000

    add animation to main page"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,20 @@
-
-// MapboxGL
-mapboxgl.accessToken =
-    'pk.eyJ1Ijoic3ppbHZpMSIsImEiOiJjbGR4Z2M5YzEwaDVkNDBwaGcwOWIzcHg4In0.PTFFlTTPfA3PnnA01vzcZw';
+ // Mapbox GL JS object
+ mapboxgl.accessToken =
+ 'pk.eyJ1Ijoic3ppbHZpMSIsImEiOiJjbGR4Z2M5YzEwaDVkNDBwaGcwOWIzcHg4In0.PTFFlTTPfA3PnnA01vzcZw';
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/szilvi1/cldvz9vlb000y01qrbvjld10b',
-    projection: 'globe', // Display the map as a globe, since satellite-v9 defaults to Mercator
-    zoom: 1.5,
-    center: [0, 40],
+ container: 'map',
+ style: 'mapbox://styles/szilvi1/cldvz9vlb000y01qrbvjld10b', // use my own style
+ projection: 'globe', // Display the map as a globe
+ zoom: 1,
+ center: [30, 40],
 });
 
 map.on('style.load', () => {
-    map.setFog({}); // Set the default atmosphere style
+ map.setFog({}); // make Earth's atmosphere lighter
 });
 
+$(document).ready(function () {
+   $('.map').addClass('makeVisible');
+    $('.mainTitle').addClass('makeVisible');
+    $('.btnPlay').addClass('makeVisible')
+})
