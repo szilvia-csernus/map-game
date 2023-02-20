df2f26815b45da579b53c16fac4159b3aae358a0
Szilvia Csernus <csernus.szilvi@gmail.com>
2023-02-20 06:52:02 +0000

    add country-boundaries-v1 tileset

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,7 @@ map.on('load', () => {
     map.addSource('country-boundaries', {
         type: 'vector',
         url: 'mapbox://mapbox.country-boundaries-v1',
+        generateId: true
     });
 
     // my country data for capitals
@@ -24,19 +25,34 @@ map.on('load', () => {
     // });
 
     map.addLayer({
-        filter: ['has', 'color_group'],
+        // filter: ['has', 'color_group'],
         id: "my-country-fill",
         paint: {
             'fill-color': [
-                "interpolate",
-                ["exponential", 1],
-                ["get", "color_group"],
-                1,
-                "hsl(295, 78%, 73%)",
-                3,
-                "hsl(44, 86%, 60%)",
-                7,
-                "hsl(235, 86%, 60%)",
+                'case',
+                ['boolean', ['feature-state', 'hover'], false],
+                "#fff",
+                // "#9BDEAC", // fill it green on hover
+                [
+                    "interpolate",
+                    ["exponential", 1],
+                    ["get", "color_group"],
+                    1,
+                    "#845EC2", // violet
+                    2,
+                    "#F7C8A8", // peach
+                    3,
+                    "#FF6F91", // fuchsia
+                    4,
+                    
+                    "#D65DB1", // magenta
+                    6,
+                    "#FFC75F", // yellow
+                    7,
+                    "#FF9671", // orange
+    
+                ],
+                
             ]
         },
         source: "country-boundaries",
@@ -44,17 +60,17 @@ map.on('load', () => {
         type: "fill"
     })
 
-    // map.addLayer({
-    //     id: "my-countries-line",
-    //     paint: {
-    //         'line-color': "#fff",
-    //         'line-width': 0.5
-            
-    //     },
-    //     source: "country-boundaries",
-    //     'source-layer': "country_boundaries",
-    //     type: "line"
-    // })
+    map.addLayer({
+        id: "my-countries-line",
+        paint: {
+            'line-color': "#fff",
+            'line-width': 0.5
+
+        },
+        source: "country-boundaries",
+        'source-layer': "country_boundaries",
+        type: "line"
+    })
 
     // map.addLayer({
 
@@ -67,33 +83,33 @@ map.on('load', () => {
     //     type: "fill"
     // })
 
-    
 
-   
+
+
     // add source for simplified country boundaries
 
-    map.addSource('countries', {
-        type: 'geojson',
-        data: '../../assets/data/countries-new.geojson',
-        generateId: true
-    });
+    // map.addSource('countries', {
+    //     type: 'geojson',
+    //     data: '../../assets/data/countries-new.geojson',
+    //     generateId: true
+    // });
 
 
-    map.addLayer({
-        id: 'country-fill',
-        type: 'fill',
-        source: 'countries',
-        layout: {},
-        paint: {
-            'fill-color': '#627BC1',
-            'fill-opacity': [
-                'case',
-                ['boolean', ['feature-state', 'hover'], false],
-                0.5,
-                0
-            ]
-        }
-    });
+    // map.addLayer({
+    //     id: 'country-fill',
+    //     type: 'fill',
+    //     source: 'countries',
+    //     layout: {},
+    //     paint: {
+    //         'fill-color': '#627BC1',
+    //         'fill-opacity': [
+    //             'case',
+    //             ['boolean', ['feature-state', 'hover'], false],
+    //             0.5,
+    //             0
+    //         ]
+    //     }
+    // });
 
     // map.addLayer({
     //     id: 'country-borders',
@@ -113,91 +129,67 @@ map.on('load', () => {
     // })
 
 
-    // map.addSource('states', {
-    //     'type': 'geojson',
-    //     'data': 'https://docs.mapbox.com/mapbox-gl-js/assets/us_states.geojson'
-    // });
-
-    // map.addLayer({
-    //     'id': 'state-fill',
-    //     'type': 'fill',
-    //     'source': 'states',
-    //     'layout': {},
-    //     'paint': {
-    //         'fill-color': '#627BC1',
-    //         'fill-opacity': [
-    //             'case',
-    //             ['boolean', ['feature-state', 'hover'], false],
-    //             1,
-    //             0.5
-    //         ]
-    //     }
-    // });
-
-    
-    // map.addLayer({
-    //     'id': 'state-borders',
-    //     'type': 'line',
-    //     'source': 'states',
-    //     'layout': {},
-    //     'paint': {
-    //         'line-color': '#627BC1',
-    //         'line-width': 1
-    //     }
-    // });
-
     // When the user moves their mouse over the state-fill layer, we'll update the
     // feature state for the feature under the mouse.
-    
+
 
     let hoveredStateId = null;
 
-    map.on('mousemove', 'country-fill', (e) => {
+    map.on('mousemove', 'my-country-fill', (e) => {
         map.getCanvas().style.cursor = 'pointer';
-        console.log(e.features[0].properties.name, e.features[0].id, hoveredStateId)
+        console.log(e.features[0].properties.name_en)
         if (e.features.length > 0) {
             if (hoveredStateId) {
-                
+
                 map.setFeatureState({
-                    source: 'countries',
+                    source: 'country-boundaries',
+                    sourceLayer: 'country_boundaries',
                     id: hoveredStateId
                 }, {
                     hover: false
                 });
-            } 
-            
+                console.log(map.getFeatureState({
+                    source: 'country-boundaries',
+                    sourceLayer: 'country_boundaries',
+                    id: hoveredStateId
+                }))
+            }
+
             hoveredStateId = e.features[0].id;
             map.setFeatureState({
-                source: 'countries',
+                source: 'country-boundaries',
+                sourceLayer: 'country_boundaries',
                 id: hoveredStateId
             }, {
                 hover: true
             });
-        
+            console.log(map.getFeatureState({
+                source: 'country-boundaries',
+                sourceLayer: 'country_boundaries',
+                id: hoveredStateId
+            }))
+
         }
     });
 
     // When the mouse leaves the state-fill layer, update the feature state of the
     // previously hovered feature.
-    map.on('mouseleave', 'country-fill', () => {
+    map.on('mouseleave', 'my-country-fill', () => {
         console.log(hoveredStateId)
         if (hoveredStateId) {
-            // map.removeFeatureState({
-            //     source: 'countries',
-            //     id: hoveredStateId
-            // }, );
+
             map.setFeatureState({
-                source: 'countries',
+                source: 'country-boundaries',
+                sourceLayer: 'country_boundaries',
                 id: hoveredStateId
             }, {
                 hover: false
             });
             hoveredStateId = null;
         }
-       
+
         map.getCanvas().style.cursor = '';
     });
-
 });
 
 
